96a6da491ec4ceacbfab4b565cd54cfda342243a
kayanCardoso <kayanlg19@gmail.com>
2023-09-14 20:39:32 -0300

    mais duas tabelas

--- Script de criaçao.sql
+++ Script de criaçao.sql
@@ -29,5 +29,33 @@ create table camisa
 (num_camisa int primary key not null
 );
 
+create table classificacao
+(posicao_tabela int primary key not null,
+quantidade_partida tinyint  not null,
+pontos tinyint not null,
+num_derrotas tinyint not null,
+num_vitoria tinyint not null,
+num_empates tinyint not null,
+gols_pro smallint not null,
+gols_contra smallint not null,
+fk_campeonato int not null,
+foreign key (fk_campeonato) references campeonato(id_campeonato) 
+);
+
+create table jogo 
+(id_jogo int primary key not null,
+estadio varchar(80) not null,
+resultado varchar(50) not null,
+data_jogo date not null,
+tipo_participante enum('Mandante', 'Visitante'),
+cidade varchar(80) not null,
+estado varchar(80) not null,
+pais varchar(80) not null,
+historico binary,
+contexto_jogo text,
+fk_campeonato int not null,
+foreign key (fk_campeonato) references campeonato(id_campeonato)
+ );
+
 
 

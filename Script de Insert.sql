f13a60477f2f2150a0cbf0d7408186a9ca639767
kayanCardoso <kayanlg19@gmail.com>
2023-09-17 10:36:46 -0300

    primeiro insert

---
+++ Script de Insert.sql
@@ -0,0 +1,64 @@
+use db_botafogo;
+
+insert jogador(id_jogador, nome) values
+	(1,'Lucas Pereira da Silva'),
+    (2, 'Lucas Pereira Silva'),
+    (3, 'Mateus Santos Oliveira'),
+    (4, 'Guilherme Fernandes Costa'),
+    (5, 'João Almeida Santos'),
+    (6, 'André Rodrigues Lima'),
+    (7, 'Rodrigo Gomes Pereira'),
+    (8, 'Gabriel Costa Alves'),
+    (9, 'Leonardo Silva Santos'),
+    (10, 'Alexandre Oliveira Souza'),
+    (11, 'Daniel Pereira Lima'),
+    (12, 'Fernando Santos Ferreira'),
+    (13, 'Caio Alves Rodrigues'),
+    (14, 'Rafael Lima Gonçalves'),
+    (15, 'Eduardo Fernandes Oliveira'),
+    (16, 'Victor Pereira Almeida'),
+    (17, 'Gustavo Silva Costa'),
+    (18, 'Bruno Ferreira Alves'),
+    (19, 'Marcos Lima Rodrigues'),
+    (20, 'Marcelo Almeida Costa'),
+    (21, 'Thiago Oliveira Pereira'),
+    (22, 'Felipe Costa Fernandes'),
+    (23, 'Luiz Rodrigues Almeida'),
+    (24, 'Vinícius Santos Oliveira'),
+    (25, 'Diego Lima Ferreira'),
+    (26, 'Carlos Almeida Gonçalves'),
+    (27, 'Lucas Fernandes Silva'),
+    (28, 'Matheus Pereira Oliveira'),
+    (29, 'Ricardo Lima Santos'),
+    (30, 'Samuel Almeida Rodrigues'),
+    (31, 'André Costa Fernandes'),
+    (32, 'César Silva Gonçalves'),
+    (33, 'Otávio Oliveira Lima'),
+    (34, 'Henrique Fernandes Costa'),
+    (35, 'Jorge Santos Pereira'),
+    (36, 'Ivan Lima Alves'),
+    (37, 'Renato Almeida Silva'),
+    (38, 'Antonio Rodrigues Costa'),
+    (39, 'Joaquim Costa Oliveira'),
+    (40, 'Nicolas Alves Ferreira'),
+    (41, 'Augusto Lima Santos'),
+    (42, 'Raul Fernandes Almeida'),
+    (43, 'Miguel Almeida Costa'),
+    (44, 'Valter Pereira Lima'),
+    (45, 'Pedro Lima Rodrigues'),
+    (46, 'Isaque Oliveira Costa'),
+    (47, 'Alexandre Santos Alves'),
+    (48, 'Rodrigo Almeida Ferreira'),
+    (49, 'Artur Fernandes Oliveira'),
+    (50, 'Bruno Silva Lima'),
+    (51, 'Felipe Costa Almeida');
+    
+   
+
+
+    
+    select * from jogador;
+
+
+
+
